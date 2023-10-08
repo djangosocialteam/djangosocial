5e5eaaa9095a79f1538e245ca8bebf62cc7a7cf9
The Epic <theepic.dev.83@gmail.com>
2023-10-08 19:23:17 +0300

    feat: add basic home page

--- django/djangosocial/home/models.py
+++ django/djangosocial/home/models.py
@@ -1,7 +1,23 @@
-from django.db import models
+"""Home page models."""
 
+from wagtail import blocks
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    """Home page model."""
+
+    body = StreamField(
+        [
+            ("rich_text", blocks.RichTextBlock()),
+        ],
+        use_json_field=True,
+        null=True,
+    )
+
+    content_panels = (
+        *Page.content_panels,
+        FieldPanel("body"),
+    )

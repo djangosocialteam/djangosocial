38f51a82f74bc9bd7c70c782f280da499655be4f
Mark Walker <mark@marksweb.co.uk>
2023-10-19 17:14:30 +0100

    fix: Remove accounts urls [#13]

--- django/djangosocial/urls.py
+++ django/djangosocial/urls.py
@@ -11,7 +11,6 @@ from djangosocial.search import views as search_views
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),

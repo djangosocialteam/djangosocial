efd79fadb73172a7fe71d9a7b876fcd89a59b21f
Andrew Miller <info@akmiller.co.uk>
2023-10-20 09:35:38 +0100

    Added nav bar and WIP on footer

--- django/djangosocial/home/models.py
+++ django/djangosocial/home/models.py
@@ -1,9 +1,15 @@
 """Home page models."""
 
+from django.db import models
+
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+
 from wagtail import blocks
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.fields import StreamField
 from wagtail.models import Page
+from wagtail.snippets.models import register_snippet
 
 from djangosocial.streams.blocks import HeroImageBlock
 
@@ -47,3 +53,31 @@ class AboutPage(Page):
 
     parent_page_types = ['home.HomePage']
     subpage_types = []
+
+
+@register_snippet
+class Footer(ClusterableModel):
+    panels = [
+        InlinePanel('footer_links', label="Footer Links")
+    ]
+
+@register_snippet
+class NavBar(ClusterableModel):
+    logo = models.ImageField(upload_to="images/logos/")
+
+    panels = [
+        FieldPanel('logo'),
+        InlinePanel('nav_links', label="Nav Links")
+    ]
+
+class Link(models.Model):
+    nav_bar = ParentalKey(NavBar, related_name='nav_links', null=True, blank=True)
+    footer = ParentalKey(Footer, related_name='footer_links',null=True, blank=True)
+
+    text = models.CharField(max_length=255)
+    url = models.URLField()
+
+    panels = [
+        FieldPanel("url"),
+        FieldPanel("text"),
+    ]

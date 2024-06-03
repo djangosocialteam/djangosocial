a9c13c032a0f76fad3e93608390f0676ca9afa13
Andrew Miller <info@akmiller.co.uk>
2024-06-03 09:22:29 +0100

    update colours in the templates to green

--- djangosocial/tailwind.config.js
+++ djangosocial/tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     extend: {
       colors: {
         primary: "#0c4b33",
+        green: {
+            '50': '#edfcf3',
+            '100': '#d4f7e1',
+            '200': '#adedc9',
+            '300': '#77dea9',
+            '400': '#40c786',
+            '500': '#1dac6c',
+            '600': '#108b57',
+            '700': '#0d6f48',
+            '800': '#0d583a',
+            '900': '#0c4b33',
+            '950': '#05291c',
+        },
       },
       spacing: {
         initial: "initial",
